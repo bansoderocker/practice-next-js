2054b0e433f6f6fa747f4c9d10b26a0de84804d0
Vijay Bansode <bansode.vijay1996@gmail.com>
2024-05-09 19:42:12 +0530

    code changes

---
+++ src/app/Transaction/ExpenseTypeList.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import _party from "../Interface/Party";
+import { database } from "../firebaseConfig";
+import { get, ref } from "firebase/database";
+import "bootstrap/dist/css/bootstrap.css";
+import editParty from "./page";
+import Lookup from "../Interface/Lookup";
+
+// async function getPartyList() {}
+
+// export default
+const PartyList = () => {
+  const [partys, setPartys] = useState<Lookup[]>([]);
+  useEffect(() => {
+    const partyRef = ref(database, "expenseType");
+    get(partyRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          var partyGridList: Lookup[] = [];
+          snapshot.forEach(function (childSnapshot) {
+            var childData = childSnapshot.val();
+            partyGridList.push(childData);
+          });
+          setPartys(partyGridList);
+          console.log(partyGridList);
+        } else {
+          console.log("no data found");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (partys.length > 0) {
+    return partys.map((x, index) => (
+      <tr key={index}>
+        <td>{index + 1}</td>
+        <td>{x.Text}</td>
+        <td>
+          <button
+            className="from-control btn btn-warning"
+            style={{ marginRight: "10px" }}
+            //  onClick={(event) => editParty(x)}
+          >
+            Edit
+          </button>
+          <button className="from-control btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    ));
+  } else {
+    <h1>no records found</h1>;
+  }
+};
+
+export default PartyList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,34 +11,34 @@ import _party from "./Interface/Party";
 export default function Home() {
   // const [user, setUser] = useState([]);
 
-  const [truck, setTruck] = useState<_truck>({
-    key: "",
-    TruckNumber: "",
-    CreatedOn: "",
-  });
+  // const [truck, setTruck] = useState<_truck>({
+  //   key: "",
+  //   TruckNumber: "",
+  //   CreatedOn: "",
+  // });
 
-  const [inputs, setInputs] = useState<_expense>({
-    key: "",
-    searchText: "",
-    tempExpenseName: "",
-    tempExpenseAmount: 0,
-    amount: 0,
-    Particular: "",
-    ParticularFrom: "",
-    ParticularTo: "",
-    TripAmount: 0,
-    TruckNumber: "",
-    BillName: "",
-    BillNumber: "",
-    Remark: "",
-    CreatedOn: "",
-    TripDate: format(new Date(), "dd-MM-yyyy"),
-    newParty: { key: "", CreatedOn: "", pageNumber: 0, partyName: "" },
-    Expense: [],
-    partyKey: "",
-    filterPartyName: "",
-    pageStatus: 0,
-  });
+  // const [inputs, setInputs] = useState<_expense>({
+  //   key: "",
+  //   searchText: "",
+  //   tempExpenseName: "",
+  //   tempExpenseAmount: 0,
+  //   amount: 0,
+  //   Particular: "",
+  //   ParticularFrom: "",
+  //   ParticularTo: "",
+  //   TripAmount: 0,
+  //   TruckNumber: "",
+  //   BillName: "",
+  //   BillNumber: "",
+  //   Remark: "",
+  //   CreatedOn: "",
+  //   TripDate: format(new Date(), "dd-MM-yyyy"),
+  //   newParty: { key: "", CreatedOn: "", pageNumber: 0, partyName: "" },
+  //   Expense: [],
+  //   partyKey: "",
+  //   filterPartyName: "",
+  //   pageStatus: 0,
+  // });
   return (
     <main className={styles.main}>
       <div className={styles.description}></div>
